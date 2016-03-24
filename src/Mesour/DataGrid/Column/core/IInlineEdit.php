9bfe8fce705059b3e2d9916598ad83991b092663
mesour <matous.nemec@gmail.com>
2016-03-24 17:58:46 +0100

    Changed copyright year

--- src/Mesour/DataGrid/Column/core/IInlineEdit.php
+++ src/Mesour/DataGrid/Column/core/IInlineEdit.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the Mesour DataGrid (http://grid.mesour.com)
  *
- * Copyright (c) 2015 Matouš Němec (http://mesour.com)
+ * Copyright (c) 2015-2016 Matouš Němec (http://mesour.com)
  *
  * For full licence and copyright please view the file licence.md in root of this project
  */
